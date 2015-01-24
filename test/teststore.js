911fc0a5eb56fae8745002cfa76856330a009361
moddy3d <moddy3d@gmail.com>
2015-01-24 22:26:29 +0800

    First working unit test, however using uuid.v4 as image key does not seem to work

--- test/teststore.js
+++ test/teststore.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+var uuid = require("node-uuid");
 var Store = require("../db/store.js");
 
 function random() {
@@ -11,24 +12,77 @@ function random() {
     return id;
 }
 
+function compareArrays(arrayA, arrayB) {
+
+    function is(a, b) {
+        return a === b && (a !== 0 || 1 / a === 1 / b)
+               || a !== a && b !== b; 
+    }
+
+    if (arrayA.length == arrayB.length && arrayA.every(function(u, i) {
+            return is(u, arrayB[i]);
+        })
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 module.exports = {
 
     setUp: function (callback) {
-        var config = JSON.parse(fs.readFileSync("../config.json"));
+        var config = JSON.parse(fs.readFileSync(__dirname + "/../config.json"));
         this.store = new Store(config.cassandra);
         this.keyspace = config.cassandra.keyspace + "_" + random();
-
+        
+        console.log("Setting up keyspace " + this.keyspace);
         this.store.setup(this.keyspace, function () {
             callback();
         });
     },
 
     tearDown: function (callback) {
-        var this_ = this;
         this.store.shutdown();
         callback();
     },
+    
+    testAddImage: function (test) {
+            
+        var this_ = this,
+            id = 'image_id',
+            user = "foo",
+            tags = ["tag1", "tag2"],
+            type = "image/jpeg";
+    
+        try  {
+            var data = fs.readFileSync(__dirname + "/image.jpg");
+        } catch (e) {
+            console.log(e.message);
+            throw Error(e);
+        }
 
+        console.log("creating image with id " + id);
+
+        this.store.addImage(id, user, tags, data, type, getImage);
+
+        function getImage() {
+            setTimeout( function () {
+                this_.store.getImage(id, verifyImage);
+            }, 1000);
+        }
+
+        function verifyImage( image ) {
+            test.equals(image.id, id);
+            test.equals(image.user, user);
+            test.ok(compareArrays(image.tags, tags));
+            test.equals(image.type, type);
+            test.equals(image.data.toString(), data.toString());
+            test.done();
+        };
+    }
+
+    /*
     testRemoveImage: function (test) {
         test.ok(true, "true");
         test.done();
@@ -58,5 +112,6 @@ module.exports = {
         test.ok("yes");
         test.done();
     },
+    */
 };
 

28b87d2e6556c9a43df3f0c59176c638d2aecb60
moddy3d <moddy3d@gmail.com>
2015-01-24 20:30:54 +0800

    Setup templates for testing

---
+++ test/teststore.js
@@ -0,0 +1,29 @@
+var Store = require("../db/store.js");
+
+module.testAddImage = function (test) {
+    test.done();
+};
+
+module.testRemoveImage = function (test) {
+    test.done();
+};
+
+module.testGetImageByTag = function (test) {
+    test.done();
+};
+
+module.testGetImagesByTag = function (test) {
+    test.done();
+};
+
+module.testAddTags = function (test) {
+    test.done();
+};
+
+module.testRemoveTags = function (test) {
+    test.done();
+};
+
+module.testRemoveTags = function (test) {
+    test.done();
+};

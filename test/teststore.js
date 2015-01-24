4549c236e74432b568efc21593ab368bc5fe341c
moddy3d <moddy3d@gmail.com>
2015-01-24 21:00:43 +0800

    Corrected testing framework... forgot we're not using nosetests

--- test/teststore.js
+++ test/teststore.js
@@ -1,29 +1,62 @@
+var fs = require("fs");
 var Store = require("../db/store.js");
 
-module.testAddImage = function (test) {
-    test.done();
+function random() {
+    var id = "";
+    var characters = "0123456789";
+
+    for (var i = 0; i < 5; i++)
+        id += characters.charAt(Math.floor(Math.random() * characters.length));
+
+    return id;
+}
+
+module.exports = {
+
+    setUp: function (callback) {
+        var config = JSON.parse(fs.readFileSync("../config.json"));
+        this.store = new Store(config.cassandra);
+        this.keyspace = config.cassandra.keyspace + "_" + random();
+
+        this.store.setup(this.keyspace, function () {
+            callback();
+        });
+    },
+
+    tearDown: function (callback) {
+        var this_ = this;
+        this.store.shutdown();
+        callback();
+    },
+
+    testRemoveImage: function (test) {
+        test.ok(true, "true");
+        test.done();
+    },
+
+    testGetImageByTag: function (test) {
+        test.ok(true, "true");
+        test.done();
+    },
+
+    testGetImagesByTag: function (test) {
+        test.ok(true, "true");
+        test.done();
+    },
+
+    testAddTags: function (test) {
+        test.ok("yes");
+        test.done();
+    },
+
+    testRemoveTags: function (test) {
+        test.ok("yes");
+        test.done();
+    },
+
+    testRemoveTags: function (test) {
+        test.ok("yes");
+        test.done();
+    },
 };
 
-module.testRemoveImage = function (test) {
-    test.done();
-};
-
-module.testGetImageByTag = function (test) {
-    test.done();
-};
-
-module.testGetImagesByTag = function (test) {
-    test.done();
-};
-
-module.testAddTags = function (test) {
-    test.done();
-};
-
-module.testRemoveTags = function (test) {
-    test.done();
-};
-
-module.testRemoveTags = function (test) {
-    test.done();
-};

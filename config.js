0b2b15a947e1067e294fd445c982ac72078c5c4f
moddy3d <moddy3d@gmail.com>
2015-01-27 21:39:23 +0800

    Made config helper module more robust by concat path

--- config.js
+++ config.js
@@ -4,8 +4,9 @@
  * Helper for loading the configuration
  */
 
-var fs = require("fs");
+var fs = require("fs"),
+    path = require("path");
 
 module.exports = function () {
-    return JSON.parse(fs.readFileSync("config.json"));
+    return JSON.parse(fs.readFileSync(path.join(__dirname, "config.json")));
 };

fdd66c788976d30cecfac3615793dfe0f1492f2c
moddy3d <moddy3d@gmail.com>
2015-01-29 19:42:06 +0800

    Restructured get images to handle both json and image requests, and added post handler for creating new images

--- routes/images.js
+++ routes/images.js
@@ -2,18 +2,34 @@ var express = require('express');
 var async = require('async');
 var router = express.Router();
 
-/* GET images */
-router.get(/\/(.*)/, function(req, res, next) {
-    var id = req.params[0];
-    async.waterfall([
-        function (done) {
-            var image = res.locals.store.getImage(id, done);
-        },
-    ], function (error, image) {
-        if (error || !image) return next();
-        res.set('Content-Type', image.type);
-        res.send(image.data);
+router.route('')
+    .post(function (req, res, next) {
+        var image = req.body;
+        image.data = new Buffer(image.data);
+        res.locals.store.addImage(image.id, image.user, image.tags, 
+                                  image.data, image.type, function (error, results) {
+            if (error) return next();
+            res.end();
+        });
     });
-});
+
+router.route('/:image_id')
+
+    /* GET image by id */
+    .get(function(req, res, next) {
+        var id = req.params.image_id;
+
+        res.locals.store.getImage(id, function (error, image) {
+            if (error || !image) return next();
+
+            if (req.headers.accept == 'application/json') {
+                delete image.data;
+                res.json(image);
+            } else {
+                res.set('Content-Type', image.type);
+                res.send(image.data);
+            }
+        });
+    });
 
 module.exports = router;
